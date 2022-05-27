9484fe6b9465603dd20025640e187b13cdefcd09
Simon Xu <xulan20907@gmail.com>
2022-05-27 07:57:52 -0700

    get form input and render task lit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
-console.log("test")
-var buttonEl = document.querySelector("#save-task");
+
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task infor and add to list item
+    var taskInfoEl = document.createElement("div")
+    // give it a class name
+    taskInfoEl.className = "task-info"
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>"
+
+    listItemEl.append(taskInfoEl)
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler)
+formEl.addEventListener("submit", createTaskHandler)
